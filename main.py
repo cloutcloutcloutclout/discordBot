aa669248d91f186b946fd0a952fba257ae149d33
Zain <zainhu06@hotmail.com>
2026-04-21 20:05:19 +0100

    Avatar

--- main.py
+++ main.py
@@ -40,6 +40,22 @@ async def on_message(message):
     await bot.process_commands(message) ## allows to continue handing message in the server by anyone else << 
 
 
+@bot.command
+async def avatar(ctx, member: discord.Member = None):
+    ## getting who the member is and url
+    member = member or ctx.author
+    avatar_url = member.display_avatar.url
+    ## embed
+    embed = discord.Embed(
+        title=f"{member.name}'s Avatar,",
+        color=discord.Color.random()
+    )
+
+    embed.set_image(url=avatar_url)
+
+    await ctx.send(embed=embed)
+
+
 
 
 
