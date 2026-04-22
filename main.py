9d0a5b0cefb265c4a8156b35a10a2850040ddaf8
Zain <zainhu06@hotmail.com>
2026-04-22 18:02:53 +0100

    Comments and initializing a new racism filter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 ## importing key dependencies
-
 import discord
 from discord.ext import commands
 import logging
@@ -17,29 +16,44 @@ intents.message_content = True
 intents.members = True
 
 ## Bot
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-bot = commands.Bot(command_prefix='!', intents=intents)
-
+## bot initialize
 @bot.event
 async def on_ready():
-    print(f"{bot.user.name} is initializing...")
+    print(f"{bot.user.name} is online.")
 
+
+## if someone joins the server
 @bot.event
 async def on_member_join(member):
     await member.send(f"Welcome to the server {member.name}")
 
+
+## user messaging
 @bot.event
 async def on_message(message):
+
+    ## if author is bot ignore
     if message.author == bot.user:
         return
     
+    # racism filter {progress}
     if "shit" in message.content.lower():
         await message.delete()
         await message.channel.send(f"{message.author.mention} swear is filtered")
 
+
+
+
+
+
+
+
+
     await bot.process_commands(message) ## allows to continue handing message in the server by anyone else << 
 
-
+## Users avatar
 @bot.command()
 async def avatar(ctx, member: discord.Member = None):
     ## getting who the member is and url
@@ -54,5 +68,13 @@ async def avatar(ctx, member: discord.Member = None):
     embed.set_image(url=avatar_url)
 
     await ctx.send(embed=embed)
+
+
+
+
+
+
+
+
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
