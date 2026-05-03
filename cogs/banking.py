9273f33a5640d74ba348449c6d575e50fbe0826f
Zain <zainhu06@hotmail.com>
2026-05-03 21:47:11 +0100

    json and bank py INIT

---
+++ cogs/banking.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+import os
+import json
+
+'''
+Banking
+
+Users all start with $0
+
+Command:
+    $daily -> every 24 hours get $500
+    $shop ? 
+    $pay {user} {amount}
+    $cf, $coinflip -> spin a coin -> heads / tails {betting ammount} -> random 0,1 if 0 - head , 1 tails
+'''
